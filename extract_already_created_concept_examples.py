969f174ad63032a207960f17e1dc0e5ad85414da
Mateogit32 <36247420+mateo3264@users.noreply.github.com>
2024-06-08 12:11:06 -0500

    added try except in case there is no concept in file

--- extract_already_created_concept_examples.py
+++ extract_already_created_concept_examples.py
@@ -2,10 +2,13 @@ import re
 
 
 def get_titles_of_examples(file_data, concept):
-    cai = file_data[concept]
+    try:
+        cai = file_data[concept]
 
-    pattern = '\*\*(.*?)\*\*' 
+        pattern = '\*\*(.*?)\*\*' 
 
-    list_of_titles_of_examples = re.findall(pattern, cai)
+        list_of_titles_of_examples = re.findall(pattern, cai)
 
-    return list_of_titles_of_examples
+        return list_of_titles_of_examples
+    except:
+        return []
