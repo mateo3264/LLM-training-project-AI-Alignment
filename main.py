16648e80bd8b9dbece1691effcbcd4a11d1a946d
Mateogit32 <36247420+mateo3264@users.noreply.github.com>
2024-06-08 11:58:11 -0500

    added code to tell the LLM to make different examples from the ones already in. Adapted code to work after change in cli_tools.py

--- main.py
+++ main.py
@@ -24,11 +24,13 @@ import os
 import json
 import pandas as pd
 import ast
+from extract_already_created_concept_examples import get_titles_of_examples
 
 
+# llm, main_concept, format
+arguments = init_cli_tool(True)
 
-llm, main_concept = init_cli_tool(True)
-llm_inputs = LLMInputs(main_concept)
+llm_inputs = LLMInputs(arguments['main_concept'])
 
 
 
@@ -47,18 +49,18 @@ json_file = open(concept_mhas_filename)
 
 queried_concepts_with_mhas = json.load(json_file)
 
-if queried_concepts_with_mhas.get(main_concept, None) is None:
+if queried_concepts_with_mhas.get(arguments['main_concept'], None) is None:
     initial_time = time.perf_counter()
-    defining_characteristics = MHAFinder(llm_inputs).get_MHAs(llm)
+    defining_characteristics = MHAFinder(llm_inputs).get_MHAs(arguments['LLM'])
     print('Time taken to find the MHAs: ', time.perf_counter() - initial_time)
-    queried_concepts_with_mhas[main_concept] = defining_characteristics
+    queried_concepts_with_mhas[arguments['main_concept']] = defining_characteristics
     print('queried_concepts_with_mhas')
     print(queried_concepts_with_mhas)
     json_file_write = open(concept_mhas_filename, 'w')
     json.dump(queried_concepts_with_mhas, json_file_write)
 else:
     print('from already queried concept')
-    defining_characteristics = queried_concepts_with_mhas[main_concept]
+    defining_characteristics = queried_concepts_with_mhas[arguments['main_concept']]
 input('continue with examples?')
 
 
@@ -70,24 +72,56 @@ prompt_nonexamples = ChatPromptTemplate.from_template(NONEXAMPLES_TEMPLATE)
 
 
 
-chain = prompt | llm | StrOutputParser()
-chain_nonexample = prompt_nonexamples | llm | StrOutputParser()
+chain = prompt | arguments['LLM'] | StrOutputParser()
+chain_nonexample = prompt_nonexamples | arguments['LLM'] | StrOutputParser()
 
 #chair_concept = {'concept':'chair', 'characteristics':'''1- Has a back.\n2- Sits one person.\n3- Legs rest at a 90 degree angle approximately'''}
 #belief_concept = {'concept':'to believe', 'characteristics':'''1- Some event missing. 2- Action taken to affirm the likelihood of the event.'''}
 print(10*'0','defining_characteristics',10*'0')
 print(defining_characteristics)
-main_concept = {'concept':main_concept, 'defining_characteristics':defining_characteristics}
+main_concept = {'concept':arguments['main_concept'], 'defining_characteristics':defining_characteristics}
+
+
+concept_examples_filename = 'concept_examples.jsonl'
+
+with open(concept_examples_filename, 'r') as file:
+        concept_examples = json.load(file)
+
+
+existing_examples = get_titles_of_examples(concept_examples, arguments['main_concept'])
+existing_examples = '\n'.join(existing_examples)
+print('existing_examples:')
+print(existing_examples)
+print('defining_characteristics')
+print(defining_characteristics)
+
+if arguments['format'] == 'json':
+    chain_input = {
+        'concept':arguments['main_concept'],
+        'defining_characteristics':defining_characteristics,
+        'examples_format':json_examples_format,
+        'existing_examples':existing_examples
+        }
+else:
+    chain_input = {
+        'concept':arguments['main_concept'], 
+        'defining_characteristics':defining_characteristics, 
+        'examples_format':csv_examples_format,
+        'existing_examples':existing_examples
+        }
 
 with get_openai_callback() as c:
     print(15*'*', 'EXAMPLES', 15*'*')
-    examples_of_rational_set = chain.invoke(main_concept)
-    concept_examples_filename = 'concept_examples.jsonl'
+    examples_of_rational_set = chain.invoke(chain_input)
+    
 
-    with open(concept_examples_filename, 'r') as file:
-        concept_examples = json.load(file)
-    #ToDo if concept is already in json then add into its corresponding list
-    concept_examples[main_concept['concept']] = examples_of_rational_set
+    
+    
+    if concept_examples.get(main_concept['concept'], None) is not None:
+        concept_examples[main_concept['concept']] += examples_of_rational_set
+    else:
+        concept_examples[main_concept['concept']] = examples_of_rational_set
+
     
     
     # for concept in concept_examples:
@@ -101,6 +135,7 @@ with get_openai_callback() as c:
     with open(concept_examples_filename, 'w') as file:
 
         json.dump(concept_examples, file)
+        # csv.writer()
 
     
     # print(examples_of_rational_set)
